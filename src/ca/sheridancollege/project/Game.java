3e5ccfd0241838d753657ab918fa8e8599945cee
RafaeKhan <khanrafa@sheridancollege.ca>
2021-07-26 14:34:58 -0400

    Added Various Classes needed for OOP Design

--- src/ca/sheridancollege/project/Game.java
+++ src/ca/sheridancollege/project/Game.java
@@ -1,16 +1,13 @@
-/**
- * SYST 17796 Project Winter 2019 Base code.
- * Students can modify and extend to implement their game.
- * Add your name as a modifier and the date!
- */
-package GoFish;
+package Group12_CardGame;
 
 import java.util.ArrayList;
 
 /**
- * The class that models your game. You should create a more specific
- * child of this class and instantiate the methods given.
- * @author dancye, 2018
+ * The class that models your game. You should create a more specific child of this class and instantiate the methods
+ * given.
+ *
+ * @author dancye
+ * @author Paul Bonenfant Jan 2020
  */
 public abstract class Game {
 
